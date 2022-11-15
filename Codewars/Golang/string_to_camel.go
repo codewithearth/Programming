770e12b1a7ea2b220d1eab6eb8346215171b4c31
Prince <97332211+codewithearth@users.noreply.github.com>
2022-11-15 05:40:16 +0000

    update file

--- Codewars/Golang/string_to_camel.go
+++ Codewars/Golang/string_to_camel.go
@@ -1,8 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func main(){
+func main() {
 
-	fmt.Println()
+	fmt.Println(ToCamelCase("The_Stealth_Warrior"))
+}
+
+func ToCamelCase(s string) string { // convert normal string to camel case
+
+	// create two variables, one for result and one for flag
+	result := ""
+	upper := 0
+
+	for _, i := range s { //iterate over string
+
+		if i == 45 || i == 95 { //if string is underscore or hypen then continue for next iteration and add 1 into flag
+			upper = 1
+			continue
+
+		} else if upper == 1 { //if flag is 1 then uppercase the string and continue for nexr iteration and add 0 into flag
+
+			result += strings.ToUpper(string(i))
+			upper = 0
+			continue
+		}
+
+		result += string(i)
+
+	}
+
+	return result
 }
