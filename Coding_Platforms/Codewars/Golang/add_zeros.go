2e40703b83ebb12eff5e7a45ae2322e3dd971ecf
Prince <97332211+codewithearth@users.noreply.github.com>
2022-11-17 08:13:07 +0000

    update file

--- Coding_Platforms/Codewars/Golang/add_zeros.go
+++ Coding_Platforms/Codewars/Golang/add_zeros.go
@@ -1,1 +1,32 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(MoveZeros([]int{1, 2, 0, 1, 0, 1, 0, 3, 0, 1}))
+}
+
+func MoveZeros(arr []int) []int { //Move all zeros to the end
+
+	//create two slice,
+	//one is for result silce and another is for store all zeros
+	result := []int{}
+	zeros := []int{}
+
+	for _, i := range arr { //iterate over the slice
+
+		if i == 0 { //if if interger is zero then add it to zeros slice
+			zeros = append(zeros, 0)
+			continue
+		}
+		//append add non-zero integer to the result
+		result = append(result, i)
+	}
+
+	//finally we have result slice of non-zeros
+	//then add zeros slice in the end of result
+	result = append(result, zeros...)
+
+	return result
+}
