f7779ce26b2d8195f57cb33904f8d3c3d6b408ed
Prince <97332211+codewithearth@users.noreply.github.com>
2022-11-20 17:13:51 +0000

    Update file

--- Coding_Platforms/Codewars/Python/spinning_words.py
+++ Coding_Platforms/Codewars/Python/spinning_words.py
@@ -1,3 +1,20 @@
 def spin_words(sentence):
-    # Your code goes here
-    return None
+
+    #create list for store words from sentence and
+    #one veriable for index 
+    word_list = [word for word in sentence.split(" ")]
+    idx  = 0
+
+    for word in word_list: # iterate over the list
+
+        if len(word) >4: # if word lenght is greater then four then reverse the word
+
+            word_list[idx] = word[::-1]
+
+        idx +=1
+
+    # join the word of word_list by space
+    return " ".join(word_list)
+
+
+print(spin_words("i love pizza and burger"))
