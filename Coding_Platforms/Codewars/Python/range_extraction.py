a4bcbb7abc94e75a12c0b578cc310c94dfd47547
Prince <97332211+codewithearth@users.noreply.github.com>
2022-11-22 17:50:58 +0000

    Add new file

---
+++ Coding_Platforms/Codewars/Python/range_extraction.py
@@ -0,0 +1,10 @@
+def solution(args):
+
+    result = ""
+    count = 0
+    for i in args:
+        
+    # your code here
+    return result
+
+print(solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]))
