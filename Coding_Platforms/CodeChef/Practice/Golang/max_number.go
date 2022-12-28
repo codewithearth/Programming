aef839fb869eac8a5aa3ceeba38117038b23ffbc
Prince <97332211+codewithearth@users.noreply.github.com>
2022-12-28 17:36:55 +0000

    Write code

---
+++ Coding_Platforms/CodeChef/Practice/Golang/max_number.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+
+	var testcase, size int
+	var data string
+	var value = []int{}
+	fmt.Scan(&testcase)
+	for i := 0; i < testcase; i++ {
+		ans := 1
+		fmt.Scan(&size)
+		fmt.Scan(&data)
+		for _, j := range data {
+			val, _ := strconv.Atoi(string(j))
+			value = append(value, val)
+
+		}
+		fmt.Println(CheckMax(value))
+	}
+
+}
+
+func CheckMax(list []int) int {
+
+	result := 1
+
+	for _,k := range list{
+		if 
+	}
+	return result
+}
