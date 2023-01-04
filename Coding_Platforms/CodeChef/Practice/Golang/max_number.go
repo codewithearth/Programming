cb71037ede180c3d3f599d5a69029aae4553f039
Prince <97332211+codewithearth@users.noreply.github.com>
2023-01-04 17:53:22 +0000

    Update file

--- Coding_Platforms/CodeChef/Practice/Golang/max_number.go
+++ Coding_Platforms/CodeChef/Practice/Golang/max_number.go
@@ -2,34 +2,42 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func main() {
 
+	//create variables
 	var testcase, size int
 	var data string
-	var value = []int{}
-	fmt.Scan(&testcase)
-	for i := 0; i < testcase; i++ {
+
+	fmt.Scan(&testcase) //take input for number of testcases
+
+	for i := 0; i < testcase; i++ { //enter value for each test case
+
 		fmt.Scan(&size)
 		fmt.Scan(&data)
-		for _, j := range data {
-			val, _ := strconv.Atoi(string(j))
-			value = append(value, val)
 
+		var result int //create variable to store variable
+
+		list := []rune(data) //add test case data into slice
+
+		for idx, val := range list { //iterate over slice
+
+			if idx == 0 { //if value is first
+
+				if val == 49 || len(list) == 1 { // if first number is 1 or size of list is 1 then add 1 in result
+					result++
+				}
+				continue
+
+			} else if list[idx-1] == 48 && val == 49 { //if previous value is 0 and current value is 1 then add 1 to result
+				result++
+
+			} else if idx == len(list)-1 && val == 48 { //if last value is 0 then add 1 to result
+				result++
+			}
 		}
-		fmt.Println(CheckMax(value))
+		fmt.Println(result)
 	}
 
-}
-
-func CheckMax(list []int) int {
-
-	result := 1
-
-	for _, k := range list {
-		fmt.Println(k)
-	}
-	return result
 }
