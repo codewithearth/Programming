74beced33013609dba5689a7dc0049c9d9d53a25
Prince <97332211+codewithearth@users.noreply.github.com>
2023-01-13 07:25:39 +0000

    Add new file

--- Coding_Platforms/Leetcode/Practice/Golang/mini_rotation.go
+++ Coding_Platforms/Leetcode/Practice/Golang/mini_rotation.go
@@ -4,72 +4,69 @@ import "fmt"
 
 func main() {
 
-	fmt.Println(minDominoRotations([]int{3, 5, 1, 2, 3}, []int{3, 6, 3, 3, 4}))
+	fmt.Println(minDominoRotations([]int{1, 2}, []int{2, 1}))
 }
 
-func minDominoRotations(tops []int, bottoms []int) int {
+func minDominoRotations(tops []int, bottoms []int) int { // Check minimum rotation
 
-	var top_data, bottom_data int
-	tops_val, bottoms_val := max_num(tops, bottoms)
+	// check the rotation of numbers from both slice to get number
+	top_rotation := check_rotation(max_num(tops), tops, bottoms)
+	bottom_rotation := check_rotation(max_num(bottoms), bottoms, tops)
 
-	top_data = check_val(tops_val, tops, bottoms)
-	bottom_data = check_val(bottoms_val, bottoms, tops)
+	if (top_rotation <= bottom_rotation) && (top_rotation > -1) { // if tops rotation is less or equal to bottom rotation and rotation is greater then -1 then return tops rotation
+		return top_rotation
 
-	if top_data == bottom_data {
-		return top_data
-	} else if (top_data < bottom_data) && (top_data > 0) {
-		return top_data
-	} else if (bottom_data < top_data) && (bottom_data > 0) {
-		return bottom_data
+	} else if bottom_rotation > -1 { // if bottoms rotation is greater then -1 then return bottom roatation
+		return bottom_rotation
+
+	} else { //else return -1
+		return -1
 	}
 
-	return -1
 }
 
-func max_num(tops []int, bottoms []int) (int, int) {
+func max_num(list []int) int { //check max occurrence of number from slice
 
-	var max_top, top_val, max_bottom, bottom_val int
+	count := map[int]int{}
+	var max, val int
 
-	for i := 1; i <= 6; i++ {
-		if max := check_count(i, tops); max > max_top {
-			max_top = max
-			top_val = i
-		}
-		if max := check_count(i, bottoms); max > max_bottom {
-			max_bottom = max
-			bottom_val = i
+	for _, i := range list { //tereate over slice
+
+		if count[i] == 0 { // if new in slice add to map
+			count[i] = 1
+
+			if count[i] > max { // if count is greater then max then assign count to max and add that number to val
+				max = count[i]
+				val = i
+			}
+		} else { //else increase the count
+			count[i] += 1
+
+			if count[i] > max { // if count is greater then max then assign count to max and add that number to val
+				max = count[i]
+				val = i
+			}
 		}
 	}
-	return top_val, bottom_val
+
+	return val
 }
 
-func check_count(i int, list []int) int {
+func check_rotation(val int, list1 []int, list2 []int) int { //check rotation of numbers
+
 	var count int
-	for _, j := range list {
-		if i == j {
-			count++
-		}
-	}
-	return count
-}
 
-func check_val(val int, list1 []int, list2 []int) int {
-
-	res_list := []int{}
-	var count int
+	//iterate over list to check rotation
 	for idx, i := range list1 {
-		if i != val {
-			if list2[idx] == val {
-				res_list = append(res_list, list2[idx])
+
+		if i != val { //if number is not match then rotate from another list
+			if list2[idx] == val { //if match from another list then increase count
 				count++
-			} else {
+			} else { //if not match from another list then return -1
 				return -1
 			}
-		} else {
-			res_list = append(res_list, i)
 		}
 	}
 
 	return count
-
 }
