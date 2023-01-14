f507ef22bfef877f448d3abc9542ae1ca3eaed20
Prince <97332211+codewithearth@users.noreply.github.com>
2023-01-14 13:00:25 +0000

    Add new file

--- Coding_Platforms/Leetcode/Practice/Python/mini_rotation.py
+++ Coding_Platforms/Leetcode/Practice/Python/mini_rotation.py
@@ -1,8 +1,14 @@
 class Solution:
-    def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
+    def minDominoRotations(self, tops: list[int], bottoms: list[int]) -> int:
 
-        print(tops,bottoms)
-        return 1
+        for val in [tops[0],bottoms[0]]: # check the first number from both list 
 
+            if all(val in pair for pair in zip(tops, bottoms)): # if that number present in all the pair of two lists
 
+                # subtract the maximum of that number count(of both list) from length of list.
+                return len(tops) - max(tops.count(val), bottoms.count(val))
+        
+        # if both result is false then return -1
+        return -1
 
+print(Solution.minDominoRotations(Solution,[2,1,2,4,2,2],[5,2,6,2,3,2]))
