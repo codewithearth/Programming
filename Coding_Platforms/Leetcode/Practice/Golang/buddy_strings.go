388a2168bfd0bd364c1ba3f9f8cd70a88ae2d785
Prince <97332211+codewithearth@users.noreply.github.com>
2023-01-20 17:53:27 +0000

    Add new files

---
+++ Coding_Platforms/Leetcode/Practice/Golang/buddy_strings.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(buddyStrings("ab", "ba"))
+}
+
+func buddyStrings(s string, goal string) bool {
+
+	var num string
+	for idx, i := range s {
+
+		if s[idx] != goal[idx] {
+			if num == "" {
+				num = string(s[idx])
+			} else if string(goal[idx]) == num {
+				return true
+			}
+		}
+	}
+	return false
+}
